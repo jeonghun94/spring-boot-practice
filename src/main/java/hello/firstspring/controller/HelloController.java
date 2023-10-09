b1b5f9e2f9347bdff7cd261fe6c87daae1845a74
조정훈 <jeongh1021@naver.com>
2023-10-09 22:23:19 +0900

    api 호출 메소드추가

--- src/main/java/hello/firstspring/controller/HelloController.java
+++ src/main/java/hello/firstspring/controller/HelloController.java
@@ -2,38 +2,83 @@ package hello.firstspring.controller;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    
-    @GetMapping("/hello")
-    public String hello(Model model) {
+
+  @GetMapping("/hello")
+  public String hello(Model model) {
         model.addAttribute("data", "jhun");
 
-        return "hello";
+    return "hello";
+  }
+
+  @GetMapping("/hello-mvc")
+  public String helloMvc(@RequestParam String name, Model model) {
+    model.addAttribute("name", name);
+    return "hello";
+  }
+
+  @GetMapping("/json")
+  @ResponseBody
+  public Map<String, String> jsonReturnSample() {
+    Map<String, String> map = new HashMap<>();
+    map.put("name", "111");
+    map.put("age", "2222");
+    return map;
+  }
+
+  @GetMapping("/hello-string")
+  @ResponseBody
+  public String helloString(@RequestParam String name) {
+    return "hello " + name; // "hello spring"
+  }
+
+  @GetMapping("/hello-api")
+  @ResponseBody
+  public Hello helloApi(@RequestParam String name,@RequestParam String sex, @RequestParam int age) {
+    Hello hello = new Hello();
+    hello.setName(name);
+    hello.setAge(age);
+    hello.setSex(sex);
+    return hello; // {"name":"spring"}
+  }
+
+  static class Hello {
+
+    private String name;
+    private String sex;
+
+	public String getSex() {
+		return this.sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+    private int age;
+
+
+    public int getAge() {
+      return this.age;
     }
 
-    @GetMapping("/hello-mvc")
-    @ResponseBody
-    public String helloMvc(String name, Model model) {
-        model.addAttribute("name", name);
-
-
-        //
-        return "hello-template";
+    public void setAge(int age) {
+      this.age = age;
     }
-    
-    @GetMapping("/json")
-    @ResponseBody
-    public Map<String, String> jsonReturnSample(){
-        Map<String, String> map = new HashMap<>();
-        map.put("name", "111");
-        map.put("age", "2222");
-        return map;
+
+    public String getName() {
+      return this.name;
     }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+  }
 }
